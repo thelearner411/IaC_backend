3ffec699d39fb4885d51e4427a1988fb224bb930
thelearner411 <collinsmik99@gmail.com>
2022-12-19 01:12:25 +0000

    all CRUD tests

--- tests/functional/test_routes.py
+++ tests/functional/test_routes.py
@@ -29,4 +29,40 @@ def test_create_recipe(testing_client):
     response = testing_client.post('/recipes', json={'name': 'Milkshake', 'ingredients': 'milk', 'steps': 'Pour milk.'})
     assert response.status_code == 200
 
+def test_get_recipe(testing_client):
+    """
+    GIVEN a Flask application
+    WHEN the '/recipes' page is requested (GET)
+    THEN check the response is valid
+    """
+    response = testing_client.get('/recipes/1')
+    assert response.status_code == 200
+
+def test_update_recipe(testing_client):
+    """
+    GIVEN a Flask application
+    WHEN the '/recipes' page is requested (GET)
+    THEN check the response is valid
+    """
+    response = testing_client.put('/recipes/1', json={'name': 'Boiled Egg', 'ingredients': 'egg, water', 'steps': 'Place pan of water over medium heat for 5 minutes. Add egg and leave for 8 minutes before removing from fire.'})
+    assert response.status_code == 200
+
+def test_delete_account(testing_client):
+    """
+    GIVEN a Flask application
+    WHEN the '/recipe' page is requested (GET)
+    THEN check the response is valid
+    """
+    response = testing_client.delete('/recipes/1')
+    assert response.status_code == 200
+
+def test_base_path_post(testing_client):
+    """
+    GIVEN a Flask application
+    WHEN the '/recipes' page is requested (GET)
+    THEN check the response is valid
+    """
+    response = testing_client.post('/', json={'name': 'Boiled Egg', 'ingredients': 'egg, water', 'steps': 'Place pan of water over medium heat for 5 minutes. Add egg and leave for 8 minutes before removing from fire.'})
+    assert response.status_code == 405
+
 
